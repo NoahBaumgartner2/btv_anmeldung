e8f21010d092d80a1a67a7ffad4940e30b0458db
Noah Baumgartner <noah.baumgartner1@students.unibe.ch>
2026-04-08 18:53:24 +0000

    feat: Teilnehmer-Formular (Profil) erweitern

--- app/controllers/participants_controller.rb
+++ app/controllers/participants_controller.rb
@@ -80,7 +80,8 @@ class ParticipantsController < ApplicationController
   end
 
   def participant_params
-    allowed = [ :first_name, :last_name, :phone_number, :ahv_number, :date_of_birth, :gender ]
+    allowed = [ :first_name, :last_name, :phone_number, :ahv_number, :date_of_birth, :gender,
+                :street, :house_number, :zip_code, :city, :country, :nationality, :mother_tongue, :js_person_number ]
     allowed << :user_id if current_user.admin?
     params.expect(participant: allowed)
   end
