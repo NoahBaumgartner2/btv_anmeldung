5b5845ea7ec03a4a8526c47897b77680f1e94632
Noah Baumgartner <noah.baumgartner1@students.unibe.ch>
2026-04-09 11:09:15 +0000

    feat: courses ha j+s boolean

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_04_08_212505) do
+ActiveRecord::Schema[8.1].define(version: 2026_04_09_120000) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -99,6 +99,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_04_08_212505) do
     t.datetime "end_date"
     t.boolean "has_payment"
     t.boolean "has_ticketing"
+    t.boolean "is_js_training", default: false, null: false
     t.string "location"
     t.integer "max_participants"
     t.string "payment_methods", default: ["card"], null: false, array: true
