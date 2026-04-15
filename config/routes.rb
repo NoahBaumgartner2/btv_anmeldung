b804dd37d7df82744b141279682e75eab5872dfc
Noah Baumgartner <noah.baumgartner1@students.unibe.ch>
2026-04-15 19:17:28 +0000

    Fix database production config and healthcheck

--- config/routes.rb
+++ config/routes.rb
@@ -4,26 +4,27 @@ Rails.application.routes.draw do
   get  "dashboards/trainer"
   get  "dashboards/stats"
   get  "dashboards/export_participants", as: "export_participants_dashboard"
+  get "up" => "rails/health#show", as: :rails_health_check
 
-  resource :mail_setting, only: [:show, :edit, :update] do
+  resource :mail_setting, only: [ :show, :edit, :update ] do
     post :test_email
   end
 
   namespace :admin do
-    resource :payment_setting, only: [:show, :edit, :update] do
+    resource :payment_setting, only: [ :show, :edit, :update ] do
       post :test_connection
       post :sync_payments
     end
-    resource :infomaniak_setting, only: [:show, :edit, :update] do
+    resource :infomaniak_setting, only: [ :show, :edit, :update ] do
       post :test_connection
     end
-    resource :club_setting, only: [:show, :edit, :update] do
+    resource :club_setting, only: [ :show, :edit, :update ] do
       delete :destroy_logo, on: :member
     end
     resources :export_profiles, only: %i[index new create edit update destroy] do
       member { get :download }
     end
-    resource :nds, only: [:show], controller: "nds" do
+    resource :nds, only: [ :show ], controller: "nds" do
       post :export_persons
       post :import_persons
       post :export_awk
@@ -54,11 +55,11 @@ Rails.application.routes.draw do
   end
 
   # SumUp payments
-  get  '/registrations/:id/checkout_preview', to: 'payments#checkout_preview', as: 'checkout_preview_registration'
-  get  '/registrations/:id/checkout',         to: 'payments#checkout',         as: 'checkout_registration'
-  get  '/payments/success',                   to: 'payments#success',          as: 'payments_success'
-  get  '/payments/cancel',                    to: 'payments#cancel',           as: 'payments_cancel'
-  post '/webhooks/sumup',                     to: 'sumup_webhooks#create'
+  get  "/registrations/:id/checkout_preview", to: "payments#checkout_preview", as: "checkout_preview_registration"
+  get  "/registrations/:id/checkout",         to: "payments#checkout",         as: "checkout_registration"
+  get  "/payments/success",                   to: "payments#success",          as: "payments_success"
+  get  "/payments/cancel",                    to: "payments#cancel",           as: "payments_cancel"
+  post "/webhooks/sumup",                     to: "sumup_webhooks#create"
 
   resources :training_sessions do
     member do
@@ -86,14 +87,14 @@ Rails.application.routes.draw do
     end
   end
 
-  resource :account, only: [:show, :destroy] do
+  resource :account, only: [ :show, :destroy ] do
     get  :export,                on: :member
     post :subscribe_newsletter,  on: :member
     post :unsubscribe_newsletter, on: :member
   end
 
   # Dynamische CSS-Variablen (Vereinsfarben) – öffentlich, versioniert via ?v=
-  get '/club_colors.css', to: 'club_colors#show', as: :club_colors
+  get "/club_colors.css", to: "club_colors#show", as: :club_colors
 
   devise_for :users, controllers: { confirmations: "users/confirmations" }
   root "pages#home"
