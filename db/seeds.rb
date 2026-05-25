236ea0464667a044901e2f68a68ed2d650afc1a4
Noah Baumgartner <noah.baumgartner1@students.unibe.ch>
2026-05-25 17:11:47 +0000

    add admin users

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,16 +11,56 @@ Holiday.destroy_all
 PaymentSetting.destroy_all
 
 puts "👤 Erstelle Admin-Benutzer..."
-admin = User.find_or_initialize_by(email: 'admin@btv.com')
-admin.assign_attributes(
+
+reto = User.find_or_initialize_by(email: 'reto.marthaler@btvbern.ch')
+reto.assign_attributes(
   password: 'password',
   password_confirmation: 'password',
   confirmed_at: Time.current,
   admin: true,
   privacy_accepted: true
 )
-admin.save!
+reto.save!
 
-puts "✅ Fertig! Nur Admin-Login vorhanden. Du kannst nun mit der Kurserfassung beginnen."
-puts "   E-Mail:    admin@btv.com"
-puts "   Passwort:  password"
+Celine = User.find_or_initialize_by(email: 'cediethelm@hotmail.com')
+Celine.assign_attributes(
+  password: 'password',
+  password_confirmation: 'password',
+  confirmed_at: Time.current,
+  admin: true,
+  privacy_accepted: true
+)
+Celine.save!
+
+Christoph = User.find_or_initialize_by(email: 'schaerer_ch@bluewin.ch')
+Christoph.assign_attributes(
+  password: 'password',
+  password_confirmation: 'password',
+  confirmed_at: Time.current,
+  admin: true,
+  privacy_accepted: true
+)
+Christoph.save!
+
+Jasmin = User.find_or_initialize_by(email: 'jasmin.rosaabreu@gmail.com')
+Jasmin.assign_attributes(
+  password: 'password',
+  password_confirmation: 'password',
+  confirmed_at: Time.current,
+  admin: true,
+  privacy_accepted: true
+)
+Jasmin.save!
+
+Michele = User.find_or_initialize_by(email: 'michele.lorethan@bluewin.ch')
+Michele.assign_attributes(
+  password: 'password',
+  password_confirmation: 'password',
+  confirmed_at: Time.current,
+  admin: true,
+  privacy_accepted: true
+)
+Michele.save!
+
+
+puts "Admin accounts created"
